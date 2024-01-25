a0e1651b75ea3d599b76c5257942711a469436a9
Deepanshuisjod <deepanshudb1@gmail.com>
2024-01-25 14:01:44 +0530

    Modified rserver.py
    introduced threading module to handle concurrent clients , used threading.Thread

--- rserver.py
+++ rserver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import socket
+import threading
 
 SERVER, PORT = socket.gethostbyname(socket.gethostname()), 6379
 
@@ -7,20 +8,26 @@ rserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 rserver.bind((SERVER, PORT))
 rserver.listen()
 
+def handle_client(client_socket, client_address):
+    print(f"Connection from {client_address}")
+
+    req_msg = client_socket.recv(1024).decode('utf-8')
+    resp_msg = "+PONG\r\n"
+    count_ping = req_msg.count('PING') 
+    resp_msg = count_ping * resp_msg
+    client_socket.send(resp_msg.encode('utf-8'))
+
+    print(f"Connection from {client_address} closed")
+    client_socket.close()
+
 def main():
     print(f"Server listening on {SERVER}:{PORT}")
 
     while True:
         client_socket, client_address = rserver.accept()
-        print(f"Connection from {client_address}")
 
-        req_msg = client_socket.recv(1024).decode('utf-8')
-        resp_msg = "+PONG\r\n"
-        count_ping = req_msg.count('PING') 
-        resp_msg = count_ping * resp_msg
-        client_socket.send(resp_msg.encode('utf-8'))
-
-        client_socket.close()
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_handler.start()
 
 if __name__ == '__main__':
     main()
