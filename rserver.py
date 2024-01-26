075d5654e18a329f8666ffc524e8add645f83bfd
Deepanshuisjod <deepanshudb1@gmail.com>
2024-01-26 15:14:47 +0530

    modified rserver.py
    created separate functions for different commands and also created functions for SET & GET commands

--- rserver.py
+++ rserver.py
@@ -1,30 +1,58 @@
 #!/usr/bin/env python3
 import socket
 import threading
-
+reddis_database = {}
 SERVER, PORT = socket.gethostbyname(socket.gethostname()), 6379
 
 rserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 rserver.bind((SERVER, PORT))
 rserver.listen()
 
+# Responding to ping
+def resp_to_ping(req_msg):
+    count_ping = req_msg.count('PING')
+    resp_msg = count_ping * req_msg
+    return resp_msg
+
+# Responding to array(*) type commands
+def resp_to_array(req_msg):
+    if 'ECHO' in req_msg:
+        resp_msg = req_msg.split("\r\n")      
+        return resp_msg[4]
+
+# Responding to set command
+def resp_to_set(req_msg):
+    split_req_msg = req_msg.split("\r\n")
+    reddis_database[split_req_msg[1]] = split_req_msg[2]
+    
+# Responding to get command    
+def resp_to_get(req_msg):
+    split_req_msg = req_msg.split("\r\n")
+    if split_req_msg[1] in reddis_database:
+        return reddis_database[split_req_msg[1]]
+    else:
+        return "DNE"
+    
 def handle_client(client_socket, client_address):
     print(f"Connection from {client_address}")
 
     req_msg = client_socket.recv(1024).decode('utf-8')
-    if 'PING' in req_msg:
-        resp_msg = "+PONG\r\n"
-    elif req_msg[0] == "*":
-        print("Requested Message is array")
-        print(f"Requested message has :{req_msg[1]} string,")
-        if 'ECHO' in req_msg:
-            resp_msg = req_msg.replace('\r\n', '').replace('ECHO', '', 1)
-            resp_msg = ''.join(char for char in req_msg if not (char.isdigit() or char in '{}$*'))
-    #count_ping = req_msg.count('PING')
-    #resp_msg = count_ping * resp_msg
-    client_socket.send(resp_msg.encode('utf-8'))
+    req_msg_lower = req_msg.lower() 
 
-    print(f"Connection from {client_address} closed")
+    if 'ping' in req_msg_lower:
+        resp_msg = resp_to_ping(req_msg)
+    elif req_msg_lower.startswith("*"):
+        resp_msg = resp_to_array(req_msg)
+    elif 'set' in req_msg_lower:
+        resp_to_set(req_msg)
+        resp_msg = "OK"
+    elif 'get' in req_msg_lower:
+        resp_msg = resp_to_get(req_msg)
+    else:
+        resp_msg = "Unknown command"
+
+    client_socket.send(resp_msg.encode('utf-8'))
+    print(f"Connection from {client_address}")
     client_socket.close()
 
 def main():
@@ -32,7 +60,6 @@ def main():
 
     while True:
         client_socket, client_address = rserver.accept()
-
         client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
         client_handler.start()
 
