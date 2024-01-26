706faea6da646a4d1e927a999cc73251f3bb1929
Deepanshuisjod <deepanshudb1@gmail.com>
2024-01-26 15:15:19 +0530

    modify rclient.py

--- rclient.py
+++ rclient.py
@@ -8,7 +8,8 @@ rclient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def main():
     rclient.connect((SERVER_IP, SERVER_PORT))
 
-    ping_msg = "PING\nPING"  
+    ping_msg = "*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n"
+    ping_msg = "GET\r\nage\r\n"  
     rclient.send(ping_msg.encode('utf-8'))
 
     resp_msg = rclient.recv(1024).decode('utf-8') 
